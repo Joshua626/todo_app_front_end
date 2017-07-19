0b70b30e66fb5dddb5b9eb35218795431e0ddad4
Joshua Villanueva <joshuavillanueva2688@gmail.com>
2017-07-19 19:54:21 +0000

    organized

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     $("#submit-button").click(function(){ 
         var todoblock = $(this).closest("#new-todo-block");
-        var description =todoBlock.find("#text") ;
-        $("ul").append("<li><input type='checkbox'/> <a href='Show.html'> <span class ='todo'>" +description.val() + "</span></a></li>");
+        var description =todoblock.find("#text") ;
+        $("ul").append("<li><input type='checkbox'/><a href='show.html'><span class ='todo'>"+description.val() + "</span></a></li>");
         description.val("");
     })
 })
-    
